1448dc6e8c5e784ab3928515829dbfea1eb771d3
malec01 <mcastanot@unal.edu.co>
2024-02-28 19:28:25 -0500

    Tarea 1

---
+++ Ejercicio14.java
@@ -0,0 +1,15 @@
+package com.mycompany.ejercicio14;
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+        double numero, cuadrado, cubo;
+        numero = 3;
+        cuadrado = Math.pow(numero, 2);
+        cubo = Math.pow(numero, 3);
+        System.out.println("El número es:" + numero);
+        System.out.println("El cuadrado es:" + cuadrado);
+        System.out.println("El cubo es:" + cubo);
+              
+        
+    }
+}
---
+++ Ejercicio17.java
@@ -0,0 +1,17 @@
+package com.mycompany.ejercicio15;
+public class Ejercicio15 {
+
+    public static void main(String[] args) {
+        double radio, perimetro, area;
+        radio = 5;
+        perimetro = 2 * Math.PI * radio;
+        area = Math.PI * (Math.pow(radio , 2));
+        System.out.println("El radio es:" + radio);
+        System.out.println("La longitud de la circunferencia es:" + perimetro);
+        System.out.println("El área es:" + area);
+               
+               
+                
+                
+    }
+}
